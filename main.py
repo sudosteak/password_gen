a08ea5ce8906e46d3a28baeda5fa40163fd7ae5d
example <example@example.com>
2025-09-11 15:04:29 -0400

    Refactor main.py to simplify user input handling and remove unused key reading function

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-import sys
-import tty
-import termios
+#!/bin/python3
 from passwd_gen import gen_passwd
 from usr_gen import gen_usr
-
-def get_single_key(prompt):
-    print(prompt, end='', flush=True)
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(fd)
-        ch = sys.stdin.read(1)
-        print()
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
 
 def passwd_gen():
     infile = "./assets/wordlist.txt"
@@ -24,14 +10,10 @@ def usr_gen():
     infile = "./assets/wordlist.txt"
     print(gen_usr(infile))
 
-case = "generate (U)sername or (P)assphrase: "
-choice = get_single_key(case)
-if choice == "U" or choice == "u":
+choice = input("generate (u)sername, (p)assphrase, (q)uit: ").lower()
+if choice == "u":
     usr_gen()
-elif choice == "P" or choice == "p":
+elif choice == "p":
     passwd_gen()
 else:
     print("please enter a valid input")
-
-
-#-- add gui
